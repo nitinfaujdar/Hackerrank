cead11dc4f25301cc19ff250ee20cf27b5688b07
Nitin Faujdar <nitin.faujdar_iot18@gla.ac.in>
2019-09-24 22:13:04 +0530

    Update and rename Gravity.java to Loop.java

---
+++ src/main/Loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String[] args) {
+        int x,y;
+        y=20;
+        for (x=0; x<10; x++){
+            System.out.println("Value of x is: " + x);
+            System.out.println("Value of y is: " + y);
+            y=y-2;
+        }
+    }
+}
